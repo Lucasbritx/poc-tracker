06adc144b270636c90081a98fc3c8114b7800c1b
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-12-05 21:54:15 -0300

    WIP Ipoc interface

--- src/components/PocCard.tsx
+++ src/components/PocCard.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
-import { cn } from '@/lib/utils'
+
+interface IPoc {
+  id: string
+  nome: string
+  github_url: string
+  todo?: Array<{ title: string; done: boolean }>
+  status?: string
+  user_id?: string
+  created_at?: string
+}
 
 function PocCard({
   className,
   poc,
   ...props
 }: React.ComponentProps<'div'> & {
-  poc: any
+  poc: IPoc
 }) {
   return <div className="" {...props} >
     {poc.nome}
